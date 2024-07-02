284dda028d0a818f8dfa114f6853baa90207e4cc
Bohdan Klymeniuk <klymeniuk.bohdan@gmail.com>
2024-07-02 20:29:04 +0200

    make build

---
+++ index.a1037b53.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r){var i=document.createElement("li");i.innerText=a,t.appendChild(i),n.appendChild(t),"object"==typeof r[a]&&e(i,r[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.a1037b53.js.map
